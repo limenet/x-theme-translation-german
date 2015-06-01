7222dc00fb4452a8a54d9bb66a38c18d005f60e0
Linus Metzler <linus.metzler@gmail.com>
2015-06-01 14:20:24 +0200

    bbPress (partial), portolfio (complete)

--- x-theme-translation-german.php
+++ x-theme-translation-german.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: German Translation for the X Theme
- * Version: 0.2.5
+ * Version: 0.2.6
  * Plugin URI: https://github.com/limenet/x-theme-translation-german
  * Description: A non-official translation for the X theme into German
  * Author: Linus Metzler
