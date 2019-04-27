01aa28cec1fb966b4bf6f6f278e76d06fb3e72df
Linus Metzler <hi@linusmetzler.me>
2019-04-27 20:33:15 +0200

    remove de_CH

--- x-theme-translation-german.php
+++ x-theme-translation-german.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: German Translation for the X Theme
- * Version: 0.3.0
+ * Version: 0.3.1
  * Plugin URI: https://github.com/limenet/x-theme-translation-german
  * Description: A non-official translation for the X theme into German
  * Author: Linus Metzler
